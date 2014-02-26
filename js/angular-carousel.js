ddbd5a5bbc6c0010b876a6054f7ca54e7df0c413
michelre <remi.michel38@gmail.com>
2014-02-26 22:08:04 +0100

    add height param angular-carousel

--- js/angular-carousel.js
+++ js/angular-carousel.js
@@ -3,10 +3,15 @@ angular.module("carouselDirective", []).directive('ngCarousel', function() {
 
         var position = 0;
         var li = $(element).find("li");
-        var width;
+        var width, height;
 
         scope.$watch(attrs.ngCarousel, function(value) {
-            var widthAttr = value.width;
+            setWidthAttribute(value.width);
+            setHeightAttribute(value.height);
+            setCarousel();
+        });
+        
+        function setWidthAttribute(widthAttr){
             var widthAttrSplit = widthAttr.split(/(px|%)$/);
             switch (widthAttrSplit[1]) {
                 case "%":
@@ -16,17 +21,28 @@ angular.module("carouselDirective", []).directive('ngCarousel', function() {
                     width = widthAttrSplit[0];
                     break;
             }
-            ;
-            setCarousel();
-        });
+        };
+        
+        function setHeightAttribute(heightAttr){
+            var heightAttrSplit = heightAttr.split(/(px|%)$/);
+            switch (heightAttrSplit[1]) {
+                case "%":
+                    height = $(element).parent().width();
+                    break;
+                default:
+                    height = heightAttrSplit[0];
+                    break;
+            }
+        }
 
         function setCarousel() {
             $(element).prepend("<a href='#' class='slide-left'><span class='chevron-left'></span></a>");
             $(element).append("<a href='#' class='slide-right'><span class='chevron-right'></span></a>");
 
             $(element).css({"width": width});
+            $(element).css({"height": height});
             $(element).find("ul").css({"width": li.length * (width) + 50});
-            $(element).find("ul li").css({"width": width});
+            $(element).find("ul li").css({"width": width});            
 
 
             $(element).on("click", "a.slide-left", function(e) {
